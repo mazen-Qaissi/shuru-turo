c7acecfc39b7bb7fa1af5ac04268263d0c9bdd6b
Mazen <mazen.qaissi88@gmail.com>
2021-05-11 16:41:05 +0300

    save

--- js/user_utils.js
+++ js/user_utils.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
     // process the form
     $('#Tour_form').submit(function(event) {
         if (!$("#Tour_form").valid()) return;
@@ -11,7 +9,7 @@ $(document).ready(function() {
         $.ajax({
             type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
             url: '/createTour', // the url where we want to POST
-            // contentType: 'application/json',
+            contentType: 'application/json',
             data: JSON.stringify({
                 "id": $("#id_field").val(),
                 "start_date": $("#date_field").val(),
